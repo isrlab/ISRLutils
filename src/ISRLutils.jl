1330391865928a857bab1e760180afd7d8702526
Intelligent Systems Research Laboratory <raktim@tamu.edu>
2020-10-18 12:34:09 -0500

    initial commit

--- src/ISRLutils.jl
+++ src/ISRLutils.jl
@@ -1,5 +1,70 @@
 module ISRLutils
 
-greet() = print("Hello World!")
+# ------------------------------------------------------------
+# This file contains functions which general utility functions
+# needed for other packages.
+#
+# ------------------------------------------------------------
+
+# Convert linear index to multi-dimensional index
+# ===============================================
+function sub2ind(i,N)
+    d = length(N);
+    if d == 1
+        return i;
+    else
+        x = i-1;
+        ii = zeros(Integer,d);
+        for j in d:-1:1 
+            A =  prod(N[1:(j-1)]);
+            ii[j] = div(x,A) + 1;
+            x = x % A;
+        end
+        return ii
+    end
+end
+
+# Generate ND grid
+# ================
+function UniformNDGrid(lb,ub,N)
+    d = length(N);
+    g = [range(lb[i],ub[i],length=N[i]) for i in 1:d];
+    nmax = prod(N);
+    G = zeros(d,nmax);
+
+    for i in 1:nmax
+        ii = sub2ind(i,N);
+        for j = 1:d
+            G[j,i] = g[j][ii[j]];
+        end
+    end
+    return G
+end
+
+# Hypercube corners
+# =================
+function cornerPoints(x1,x2)
+	x1 = x1[:]; x2 = x2[:]; # Vectorize
+	d = length(x1);
+	if d!=length(x2)
+		error("dimension mismatch in x1, x2")
+	end
+
+	X = [x1 x2]; # d x 2 matrix
+	p = []; # Not the best way to do it.
+
+	ii = zeros(Int,d);
+	for i in 0:(2^d-1)
+		digits!(ii,i,base=2);
+		xx = zeros(Float64,d);
+		for j in 1:d
+			xx[j] = X[j,ii[j]+1];
+		end
+		push!(p,xx');
+	end
+	return vcat(p...);
+end
+
+
 
 end # module
