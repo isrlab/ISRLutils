0c1a7ecd0aa0ac4ef9d230ddc251cabf39bfd182
Raktim Bhattacharya <raktim@tamu.edu>
2021-01-10 10:32:59 -0600

    Minor changes.

--- src/ISRLutils.jl
+++ src/ISRLutils.jl
@@ -38,12 +38,14 @@ function GenerateNDGrid(lb,ub,N)
 
     g = [range(lb[i],ub[i],length=N[i]) for i in 1:d];
     nmax = prod(N);
-    G = zeros(d,nmax);
+    # G = zeros(d,nmax);
+    G = [];
 
     for i in 1:nmax
         ii = sub2ind(i,N);
         for j = 1:d
-            G[j,i] = g[j][ii[j]];
+            # G[j,i] = g[j][ii[j]];
+            push!(G,g[j][ii[j]]);
         end
     end
     return G
